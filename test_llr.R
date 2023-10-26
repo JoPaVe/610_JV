74c6a732a2c3d942464f64902ca83527050ee00e
Jonas Verch <jonas.verch@web.de>
2023-10-26 16:25:42 -0600

    load setup

---
+++ test_llr.R
@@ -0,0 +1,6 @@
+library("reshape2")
+data(french_fries)
+french_fries = french_fries[complete.cases(french_fries),]
+z = seq(0, 15, length.out = 100)
+fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)
+plot(z, fits)
