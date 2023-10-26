ada9364c5c600996d8206424b81af04293ac8e23
Jonas Verch <jonas.verch@web.de>
2023-10-26 16:37:00 -0600

    added benchmark_llr

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,1 +1,7 @@
-benchmark_llr
+library("reshape2")
+data(french_fries)
+french_fries = french_fries[complete.cases(french_fries),]
+z = seq(0, 15, length.out = 100)
+fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)
+
+cat(microbenchmark::microbenchmark(llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)))
