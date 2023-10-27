deec47c6e9d5966336de35c15e8b778b19cca503
Jonas Verch <jonas.verch@web.de>
2023-10-26 20:41:02 -0600

    adjusted cat

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -4,4 +4,4 @@ french_fries = french_fries[complete.cases(french_fries),]
 z = seq(0, 15, length.out = 100)
 fits = llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)
 
-cat(microbenchmark::microbenchmark(llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1)))
+microbenchmark::microbenchmark(llr(z = z, x = french_fries$potato, y = french_fries$buttery, omega = 1))
